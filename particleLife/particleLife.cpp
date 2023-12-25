f657a1d3181dc8b603eb85964678321eb18d894a
Mateusz Kwiatkowski <mateusz.kwiat316@gmail.com>
2023-12-25 18:59:01 +0100

    movement functionality for test particle

--- particleLife/particleLife.cpp
+++ particleLife/particleLife.cpp
@@ -4,27 +4,16 @@
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(800, 800), "SFML works!");
+    sf::RenderWindow window(sf::VideoMode(1500, 900), "SFML works!");
     ImGui::SFML::Init(window);
 
-    bool circleExists = true;
-    bool temp = true;
-    float circleRadius = 200.f;
+    float circleRadius = 20.f;
     int circleSegments = 100;
+
     sf::CircleShape shape(circleRadius, circleSegments);
-    sf::CircleShape shape1(circleRadius, 3);
-    sf::CircleShape shape2(circleRadius, 4);
     shape.setFillColor(sf::Color(204, 77, 5));
     shape.setOrigin(circleRadius, circleRadius);
     shape.setPosition(400, 400);
-
-    shape1.setFillColor(sf::Color(204, 77, 5));
-    shape1.setOrigin(circleRadius, circleRadius);
-    shape1.setPosition(400, 400);
-
-    shape2.setFillColor(sf::Color(204, 77, 5));
-    shape2.setOrigin(circleRadius, circleRadius);
-    shape2.setPosition(400, 400);
 
     sf::Clock deltaClock;
     while (window.isOpen())
@@ -39,39 +28,20 @@ int main()
         ImGui::SFML::Update(window, deltaClock.restart());
 
         ImGui::Begin("Settings");
-
-        static int e = 0;
-        ImGui::RadioButton("Circle", &e, 0);
-        ImGui::RadioButton("Triangle", &e, 1);
-        ImGui::RadioButton("Square", &e, 2);
-        ImGui::SliderFloat("Radius", &circleRadius, 100.f, 300.f);
-        ImGui::SliderInt("Sides", &circleSegments, 3, 150);
         ImGui::End();
 
-        shape.setRadius(circleRadius);
-        shape.setOrigin(circleRadius, circleRadius);
-        shape.setPointCount(circleSegments);
-
-        shape1.setRadius(circleRadius);
-        shape1.setOrigin(circleRadius, circleRadius);
-
-        shape2.setRadius(circleRadius);
-        shape2.setOrigin(circleRadius, circleRadius);
+        // particle movement
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::A))
+            shape.setPosition(shape.getPosition().x - 1, shape.getPosition().y);
+        else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::D))
+            shape.setPosition(shape.getPosition().x + 1, shape.getPosition().y);
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::W))
+            shape.setPosition(shape.getPosition().x, shape.getPosition().y - 1);
+        else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::S))
+            shape.setPosition(shape.getPosition().x, shape.getPosition().y + 1);
 
         window.clear(sf::Color(18, 33, 43));
-        if (circleExists) {
-
-            if (e == 0) {
-                window.draw(shape);
-            }
-            else if (e == 1) {
-                window.draw(shape1);
-            }
-            else {
-                window.draw(shape2);
-            }
-        }
-
+        window.draw(shape);
         ImGui::SFML::Render(window);
         window.display();
     }
