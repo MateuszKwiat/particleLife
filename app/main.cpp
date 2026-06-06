6bbd6eaaeb201e275242b091c076d5ab5ee1e203
MateuszKwiat <mateusz.kwiat316@gmail.com>
2026-06-06 16:57:29 +0200

    Update project dir structure
    Move app from src, make include dir.

---
+++ app/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Hello World!\n";
+    std::cout << "Hello World!\n";
+    std::cout << "Hello World!\n";
+    std::cout << "Hello World!\n";
+    std::cout << "Hello World!\n";
+    std::cout << "Hello World!\n";
+    std::cout << "Hello World!\n";
+    std::cout << "Hello World!\n";
+    std::cout << "Hello World!\n";
+    std::cout << "Hello World!\n";
+    return 0;
+}
+
+/* TODO
+ *   investigate CICD
+ *   make CICD pipeline
+ *   investigate valgrind
+ *   investigate coverage tests
+ *   add cov tests and valgrind to project
+ *   create new cmake structure
+ *   remake make commands
+ */
